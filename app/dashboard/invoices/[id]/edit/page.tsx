cb2afa440eed23f402f1b904e3c4beda6fe18aa2
Sotan Ioachim <s.ioachim@yahoo.com>
2025-11-11 15:39:10 +0200

    Fix buid errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,9 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
- 
+
+export const dynamic = 'force-dynamic';
+
 export default async function Page(props: {params: Promise<{ id: string}> }) {
   const params = await props.params;
   const id = params.id;
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,9 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
+
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const customers = await fetchCustomers();
  
